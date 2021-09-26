6b8b0c0d793850c5389b01f40106758ab6195e8c
esWhistler <83469486+esWhistler@users.noreply.github.com>
2021-09-26 01:12:40 -0300

    Update test.asm

--- test/test.asm
+++ test/test.asm
@@ -2,14 +2,14 @@
 ; R0 –> Dirección del comienzo del vector.
 ; R1 –> Cantidad de ceros que voy encontrando en el vector.
 ; R2 –> Tamaño del vector a recorrer.
-main: 	MOV R0, VECTOR ; R0 = dirección donde comienza el vector
-				MOV R1, 0x0000 ; R1 inicializado en 0
-				MOV R2, [SIZE] ; R2 = tamaño del vector.
+main:   MOV R0, VECTOR ; R0 = dirección donde comienza el vector
+        MOV R1, 0x0000 ; R1 inicializado en 0
+        MOV R2, [SIZE] ; R2 = tamaño del vector.
 ; Chequeo si el elemento del vector (apuntado por R0) es 0.
 ciclo:  CMP [R0], 0x0000
-				JNE seguir ; Salto a la etiqueta seguir si el elemento no era cero.
-				ADD R1, 0x0001 ; Encontré un cero.
+        JNE seguir ; Salto a la etiqueta seguir si el elemento no era cero.
+        ADD R1, 0x0001 ; Encontré un cero.
 seguir: ADD R0, 0x0001 ; Avanzo una posición del vector.
-				SUB R2, 0x0001 ; Decremento tamaño del vector (un elemento menos por recorrer).
-				JNE ciclo ; Si quedan elementos por recorrer retoma el ciclo
-				MOV CEROS, R1 ; Terminé de recorrer el vector y almaceno el resultado
+        SUB R2, 0x0001 ; Decremento tamaño del vector (un elemento menos por recorrer).
+        JNE ciclo ; Si quedan elementos por recorrer retoma el ciclo
+        MOV CEROS, R1 ; Terminé de recorrer el vector y almaceno el resultado
